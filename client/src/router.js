9e7d4039210272f32e51853b22025ea3d59e2b5c
Samuel Bauer <bauersam@outlook.com>
2019-03-19 11:55:53 -0600

    11:55, auth stuff apparently ok

--- client/src/router.js
+++ client/src/router.js
@@ -1,25 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Dashboard from './views/Dashboard.vue'
+import Login from './views/Login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
       path: '/',
       name: 'home',
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
-      }
-    }
+      path: '/dashboard',
+      name: 'dashboard',
+      props: true,
+      component: Dashboard
+    },
   ]
 })
