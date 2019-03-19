cfea1514678fe49eb1c212f249846642ff46cc96
Jason Molloy <jmolloy619@gmail.com>
2019-03-19 11:45:54 -0600

    11:45

--- server/server-assets/auth/session.js
+++ server/server-assets/auth/session.js
@@ -2,7 +2,7 @@ var expressSession = require("express-session");
 var mongoStore = require("connect-mongodb-session")(expressSession);
 
 var store = new mongoStore({
-  uri: "mongodb://legato:Qs3zXu9?Fe_C@den1.mongo1.gear.host:27001/legato",
+  uri: "mongodb://legato:legatopass1!@den1.mongo1.gear.host:27001/legato",
   collection: "Sessions"
 });
 
