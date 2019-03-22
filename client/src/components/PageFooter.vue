5db891e8785573a2e6a8688c38b7b612a879ca0a
Samuel Bauer <bauersam@outlook.com>
2019-03-22 12:24:21 -0600

    Ready for lunch

--- client/src/components/PageFooter.vue
+++ client/src/components/PageFooter.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="footer text-light bg-dark">
     <div class="row mx-0 px-0">
-<<<<<<< HEAD
       <div class="col-12 d-flex justify-content-center my-2">
         <i class="icon fab fa-twitter"></i><i class="icon fab fa-facebook-f ml-4"></i><i
           class="icon fab fa-instagram ml-4"></i><i class="fab fa-linkedin-in mt-2 ml-4"></i>
@@ -9,15 +8,6 @@
     </div>
     <div class="row mx-0 px-0">
       <p class="col-12 footer-text text-center mt-2">© 2019 Legato</p>
-=======
-      <div class="col-12 d-flex justify-content-center my-4">
-        <i class="icon fab fa-twitter"></i><i class="icon fab fa-facebook-f ml-5"></i><i
-          class="icon fab fa-instagram ml-5"></i><i class="fab fa-linkedin-in mt-2 ml-5"></i>
-      </div>
-    </div>
-    <div class="row mx-0 px-0">
-      <p class="col-12 footer-text text-center mt-3">© 2019 Legato</p>
->>>>>>> 90b15ae1929d9906ed94ebdae03b7a20bf486f92
     </div>
   </div>
 </template>
