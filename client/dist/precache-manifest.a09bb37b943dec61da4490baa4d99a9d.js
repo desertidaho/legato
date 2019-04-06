131a8f78e0cb1c0b1d189062e37326db1f926808
Brett Wilcox <wilcox.brett@gmail.com>
2019-04-05 21:21:01 -0600

    Post npm run build in client.

---
+++ client/dist/precache-manifest.a09bb37b943dec61da4490baa4d99a9d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "954477beb8cad5136acd",
+    "url": "/css/app.8337c3ca.css"
+  },
+  {
+    "revision": "954477beb8cad5136acd",
+    "url": "/js/app.39004b38.js"
+  },
+  {
+    "revision": "e21952505fac00e44784",
+    "url": "/css/chunk-vendors.3a477797.css"
+  },
+  {
+    "revision": "e21952505fac00e44784",
+    "url": "/js/chunk-vendors.ac4541ec.js"
+  },
+  {
+    "revision": "8869ebdd32a823ba621c43a4e22c7910",
+    "url": "/img/pic8.8869ebdd.jpg"
+  },
+  {
+    "revision": "9444da84bb0f33144ea29620f31dee17",
+    "url": "/img/pic2.9444da84.png"
+  },
+  {
+    "revision": "218ab9c73902ed37c7e3d4bd28a13b4f",
+    "url": "/img/pic5.218ab9c7.jpg"
+  },
+  {
+    "revision": "be24f387e815c2eafad8501d60b04731",
+    "url": "/img/pic6.be24f387.jpg"
+  },
+  {
+    "revision": "0b2a22cc47777b5bf60a6a2ccf5ab220",
+    "url": "/img/pic3.0b2a22cc.png"
+  },
+  {
+    "revision": "1ac63e4a7b8fbbae173d3ea2e66ce5aa",
+    "url": "/img/pic7.1ac63e4a.jpg"
+  },
+  {
+    "revision": "db0c54397194794b046d26a0f4d2d532",
+    "url": "/img/pic4.db0c5439.jpg"
+  },
+  {
+    "revision": "93c313819d58c76341fe7b7c63c3bf76",
+    "url": "/img/LogotoFull.93c31381.png"
+  },
+  {
+    "revision": "2515e4ec950ab2a2a2c7ccde0afe6ed7",
+    "url": "/img/pic1.2515e4ec.png"
+  },
+  {
+    "revision": "ede2025f636cea14a630ec42a61855ae",
+    "url": "/img/LogotoHalf.ede2025f.png"
+  },
+  {
+    "revision": "0fb0df2e9ebfc8c8f35ee230f895539d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+];
