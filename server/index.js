5774fe8b4ce6f26a4e70c19eaf50b6765fe4b3f8
Brett Wilcox <wilcox.brett@gmail.com>
2019-04-05 21:17:46 -0600

    Pre deploy changes.

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 3000
 server.use(express.static(__dirname + '/../client/dist'))
 
 
-var whitelist = ['http://localhost:8080'];
+var whitelist = ['http://localhost:8080', '//legato-bjrs.herokuapp.com'];
 var corsOptions = {
   origin: function (origin, callback) {
     var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
