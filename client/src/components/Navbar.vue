942bd1c4ba6be81ebb68f6c02202f6a01a5db9bd
Samuel Bauer <bauersam@outlook.com>
2019-04-24 16:58:28 -0600

    Tweeking nav bar. spontaeneously jumping into a complicate bootstrap heirarchy is weird.

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,52 +1,57 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-3">
-    <p class="navbar-brand mb-0 pl-2 nav-link">
-      <router-link class="nav-title mr-0 pr-0" to="/home"><img class="logo ml-0" src="@/assets/LogotoHalf.png">
-      </router-link>
-    </p><span v-if="username" class="info text-warning hello">
-      <i> {{username}}</i></span><span class="info text-danger" v-else><i>Not logged in!&nbsp</i></span>
-    <button class="navbar-toggler pr-2.5 mr-1" type="button" @click="navToggle" aria-controls="navbarSupportedContent"
-      aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <div class="row my-2 my-lg-0 d-flex justify-content-center">
-        <form @submit.prevent="search(query)" class="form-inline searchStyle">
-          <div class="col-8 p-0">
-            <input id="search-bar" autofocus="autofocus" v-model="query"
-              class="search-bar form-control mr-sm-1 text-light" type="text" placeholder=" Search..."
-              aria-label="Search">
-          </div>
-          <div class="col-2 p-0">
-            <button class="btn btn-outline-warning my-2 my-sm-0 ml-2" type="submit"><i
-                class="fas fa-search"></i></button>
-          </div>
-          <div class="col-2 p-0">
-            <button @click="clearSearch" type="reset" class="btn btn-outline-danger my-2 ml-2 my-sm-0"><i
-                class="fas fa-times"></i></button>
-          </div>
-        </form>
+  <nav class="container-fluid navbar navbar-expand-lg navbar-dark bg-dark py-3">
+    <div class="row">
+      <div class="navbar-brand mb-0 pl-2 col-md-1 col nav-link">
+        <router-link class="nav-title mr-0 pr-0" to="/home"><img class="logo ml-0" src="@/assets/LogotoHalf.png">
+        </router-link>
       </div>
-      <ul class="navbar-nav mr-auto">
-        <!-- INVESTIGATE MARGINS HERE FOR SIDE-SCROLL ISSUE-->
-        <li class="nav-item active">
-          <a class="nav-link">
-            <router-link to="/home" class=" text-light">Home
-            </router-link>
-          </a>
-        </li>
-        <li class=" nav-item active">
-          <a class="nav-link">
-            <router-link to="/dashboard" class=" text-light">
-              Dashboard</router-link>
-          </a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link">
-            <span @click="logOut" to="/" class="point text-warning">Log Out</span>
-          </a>
-        </li>
-      </ul>
+      <div class="col-md-3 col">
+        <p v-if="username" class="info text-warning hello">
+          <i> {{username}}</i></p><span class="info text-danger" v-else><i>Not logged in!&nbsp</i></span>
+        <button class="navbar-toggler pr-2.5 mr-1" type="button" @click="navToggle"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="my-2 my-md-0 d-flex justify-content-center offset-md-6 col-md-4 col">
+          <form @submit.prevent="search(query)" class="form-inline searchStyle">
+            <div class="p-0">
+              <input id="search-bar" autofocus="autofocus" v-model="query"
+                class="search-bar form-control mr-sm-1 text-light" type="text" placeholder=" Search..."
+                aria-label="Search">
+            </div>
+            <div class="p-0">
+              <button class="btn btn-outline-warning my-2 my-sm-0 ml-2" type="submit"><i
+                  class="fas fa-search"></i></button>
+            </div>
+            <div class="p-0">
+              <button @click="clearSearch" type="reset" class="btn btn-outline-danger my-2 ml-2 my-sm-0"><i
+                  class="fas fa-times"></i></button>
+            </div>
+          </form>
+        </div>
+        <ul class="row navbar-nav mr-auto">
+          <!-- INVESTIGATE MARGINS HERE FOR SIDE-SCROLL ISSUE-->
+          <li class="col-md-3 nav-item active">
+            <a class="nav-link">
+              <router-link to="/home" class=" text-light">Home
+              </router-link>
+            </a>
+          </li>
+          <li class=" nav-item active">
+            <a class="nav-link">
+              <router-link to="/dashboard" class=" text-light">
+                Dashboard</router-link>
+            </a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link">
+              <span @click="logOut" to="/" class="point text-warning">Log Out</span>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 </template>
